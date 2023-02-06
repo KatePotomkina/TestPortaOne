3340727c28e0f9413347ccb9758dd152d5e52470
kate.potomkina <kate.potomkina@gmail.com>
2023-02-06 02:01:20 +0200

    trying

--- TestPortaOne/Program.cs
+++ TestPortaOne/Program.cs
@@ -8,22 +8,16 @@ namespace TestPortaOne
     {
         public static void Main(string[] args)
         {
-         string test="The Tao gave birth to machine language.  Machine language gave birth to the assembler. The assembler gave birth to the compiler.  Now there are ten thousand Each language has its purpose, however humble.  Each languag expresses the Yin and Yang of software.  Each language has its place within the Tao. But do not program in COBOL if you can avoid it.     -- Geoffrey James, \"The Tao of Programming\" ";
-         string[] test1 = ChooseTheChar(test);
+            string text = System.IO.File.ReadAllText(@"C:\Users\Владелец\RiderProjects\TestPortaOne\TestPortaOne\Text.txt");
+
+         string[] test1 = ChooseTheChar(text);
         // Console.WriteLine( NonRepeatChar(test1));
       Console.WriteLine(NonRepeatChar(test1).ToString().First());  
         }
         public static String[] ChooseTheChar(string text)
         {
             char[] separators = new char[] { ' ', '.', '\n', '\t' };
-
             String []  exclusiveChars = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var sub in exclusiveChars)
-            {
-                Console.WriteLine($"Substring: {sub}");
-            }
-            
             return exclusiveChars;
         }
 
@@ -34,12 +28,20 @@ namespace TestPortaOne
             { 
                 char [] symbols;
                 symbols = symbol.ToCharArray();
-                exclusiveChar.Add(symbols.
-                    GroupBy(i => i)
-                    .Where(g => g
-                        .Count() == 1).
-                    Select(g => g.Key).
-                    First().ToString());
+                try
+                {
+                    exclusiveChar.Add(symbols.
+                        GroupBy(i => i)
+                        .Where(g => g
+                            .Count() == 1).
+                        Select(g => g.Key).
+                        First().ToString());
+                }
+                catch (System.InvalidOperationException)
+                {
+                    exclusiveChar.Add(null);
+            }
+                
                 
             }
                     
