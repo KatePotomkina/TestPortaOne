f9fbf156d4ba04af81fa0e02f09a8842360bf632
kate.potomkina <kate.potomkina@gmail.com>
2023-02-08 23:27:16 +0200

    Second trying

--- TestPortaOne/Program.cs
+++ TestPortaOne/Program.cs
@@ -1,54 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
-namespace TestPortaOne
+namespace TestPortaOne;
+
+internal class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            string text = System.IO.File.ReadAllText(@"C:\Users\Владелец\RiderProjects\TestPortaOne\TestPortaOne\Text.txt");
+        //var text = File.ReadAllText(@"C:\Users\Владелец\RiderProjects\TestPortaOne\TestPortaOne\Text.txt");
+       // Console.WriteLine(text);
+        string[] variad = { "hello", "kiu", "artur", "amato","honey", "hh" };/**/
+      // var test1 = ChooseTheChar(text);
+      Console.WriteLine(NonRepeatChar(variad).First());
+    }
 
-         string[] test1 = ChooseTheChar(text);
-        // Console.WriteLine( NonRepeatChar(test1));
-      Console.WriteLine(NonRepeatChar(test1).ToString().First());  
-        }
-        public static String[] ChooseTheChar(string text)
-        {
-            char[] separators = new char[] { ' ', '.', '\n', '\t' };
-            String []  exclusiveChars = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-            return exclusiveChars;
-        }
 
-        public static List<string> NonRepeatChar(string[] text)
+    public static string[] ChooseTheChar(string text)
+    {
+        var separators = new[] { ' ', '.', '\n', '\t','\0','\r','"','-' };
+        var exclusiveChars = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        return exclusiveChars;
+    }
+
+    public static List<string> NonRepeatChar(string[] text)
+    {
+        var exclusiveChar = new List<string>();
+        foreach (var symbol in text)
         {
-            List<string> exclusiveChar=new List<string>();
-            foreach (var symbol in (text))
-            { 
-                char [] symbols;
-                symbols = symbol.ToCharArray();
-                try
-                {
-                    exclusiveChar.Add(symbols.
-                        GroupBy(i => i)
-                        .Where(g => g
-                            .Count() == 1).
-                        Select(g => g.Key).
-                        First().ToString());
-                }
-                catch (System.InvalidOperationException)
-                {
-                    exclusiveChar.Add(null);
-            }
-                
-                
-            }
-                    
-                
+            char[] symbols;
+            symbols = symbol.ToCharArray();
+
+            exclusiveChar.Add(symbols.GroupBy(i => i)
+                .Where(g => g
+                    .Count() == 1).Select(g => g.Key).First().ToString());
             
-            return exclusiveChar.Distinct().ToList() ;
+            
+            
         }
         
+        return exclusiveChar;
     }
 }
