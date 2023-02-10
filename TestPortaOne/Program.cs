95f2bd3713c5ee18d69215610c6fc9bfa3fa0fc2
kate.potomkina <kate.potomkina@gmail.com>
2023-02-10 23:36:32 +0200

    The final version

--- TestPortaOne/Program.cs
+++ TestPortaOne/Program.cs
@@ -7,24 +7,30 @@ namespace TestPortaOne;
 
 internal class Program
 {
+    
     public static void Main(string[] args)
     {
-        //var text = File.ReadAllText(@"C:\Users\Владелец\RiderProjects\TestPortaOne\TestPortaOne\Text.txt");
-       // Console.WriteLine(text);
-        string[] variad = { "hello", "kiu", "artur", "amato","honey", "hh" };/**/
-      // var test1 = ChooseTheChar(text);
-      Console.WriteLine(NonRepeatChar(variad).First());
+        var text = File.ReadAllText(@"C:\Users\Владелец\RiderProjects\TestPortaOne\TestPortaOne\Text.txt");
+        var test1 = SplitTextIntoWords(text);
+        Console.WriteLine(FindUniqueElement(SplitWordIntoChar(test1)));
     }
-
-
-    public static string[] ChooseTheChar(string text)
+/// <summary>
+/// Split the text into array of words using separators
+/// </summary>
+/// <param name="text"> </param>
+/// <returns> string array (every element is a word)</returns>
+    public static string[] SplitTextIntoWords(string text)
     {
-        var separators = new[] { ' ', '.', '\n', '\t','\0','\r','"','-' };
+        var separators = new[] { ' ', '.', '\n', '\t', '\0', '\r', '"', '-','(','[',')',']' };
         var exclusiveChars = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
         return exclusiveChars;
     }
-
-    public static List<string> NonRepeatChar(string[] text)
+/// <summary>
+/// Split every element from string array to array of chars and find the first unique value from array
+/// </summary>
+/// <param name="text"></param>
+/// <returns> list of unique elements from all words </returns>
+    public static List<string> SplitWordIntoChar(string[] text)
     {
         var exclusiveChar = new List<string>();
         foreach (var symbol in text)
@@ -35,11 +41,21 @@ internal class Program
             exclusiveChar.Add(symbols.GroupBy(i => i)
                 .Where(g => g
                     .Count() == 1).Select(g => g.Key).First().ToString());
-            
-            
-            
         }
-        
+
         return exclusiveChar;
+    }
+/// <summary>
+/// To find unique value from list 
+/// </summary>
+/// <param name="arr"></param>
+/// <returns>  first unique element </returns>
+    public static string FindUniqueElement(List<string> arr)
+    {
+        var uniqueNumbers = arr.GroupBy(g => g)
+            .Where(c => c.Count() == 1)
+            .Select(k => k.Key)
+            .ToList();
+        return uniqueNumbers.First();
     }
 }
